90d85cc5483191eea4d3dea8688d46b5d269e43a
broliang <18706703437@163.com>
2019-05-08 14:30:26 +0800

    Add comments for explaining the TCP demultiplexing

--- application_layer/TCPServer.py
+++ application_layer/TCPServer.py
@@ -1,5 +1,6 @@
 from socket import *
 
+# The "welcoming socket" that waits for connection-establishment requests from TCP clients
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
@@ -8,6 +9,7 @@ serverSocket.listen(1)
 print('The server is ready to receive')
 
 while True:
+    # The server process creates a new socket for actual data transmission
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
